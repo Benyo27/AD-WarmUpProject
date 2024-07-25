c557dfbfa68fff9325f902582cc7d761c2613b79
Benyo27 <bgonzalezb@uc.cl>
2024-07-25 12:45:48 -0400

    feat(modules): config mongoose and axios

--- server/apps/server/src/app.module.ts
+++ server/apps/server/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [
+    ArticlesModule,
+    MongooseModule.forRoot('mongodb://localhost/nest6'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/apps/server/src/articles/articles.module.ts
+++ server/apps/server/src/articles/articles.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Article, ArticleSchema } from './schema/articles.schema';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
+    HttpModule,
+  ],
   controllers: [ArticlesController],
   providers: [ArticlesService],
 })
