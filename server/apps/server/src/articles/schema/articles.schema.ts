599ef8d41de0f059bef4ac5ef7ef024801513ba7
Benyo27 <bgonzalezb@uc.cl>
2024-07-25 12:46:07 -0400

    feat(schema): define articles schema

---
+++ server/apps/server/src/articles/schema/articles.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Date, HydratedDocument } from 'mongoose';
+
+export type ArticleDocument = HydratedDocument<Article>;
+
+@Schema()
+export class Article {
+  @Prop({ required: true })
+  author: string;
+
+  @Prop({ required: true })
+  created_at: Date;
+
+  @Prop({ required: true, unique: true })
+  title: string;
+
+  @Prop({ required: true })
+  url: string;
+}
+
+export const ArticleSchema = SchemaFactory.createForClass(Article);
