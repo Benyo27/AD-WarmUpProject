375176391c50de8270b5b4bec7ad8fe98fc30ab9
Benyo27 <bgonzalezb@uc.cl>
2024-07-25 23:12:20 -0400

    feat(components): create Hero component

---
+++ client/src/app/components/home/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+    return (
+        <main className={styles.Hero}>
+            <h1>HN Feed</h1>
+            <h4>We &lt;3 hacker news!</h4>
+        </main>
+    )
+}
