c5bcd0630b7d5b48ab81e0f2156850b956276c7f
Benyo27 <bgonzalezb@uc.cl>
2024-07-26 20:01:43 -0400

    feat(articles): add delete button

--- client/src/app/components/home/ArticlesContainer/ArticlesContainer.tsx
+++ client/src/app/components/home/ArticlesContainer/ArticlesContainer.tsx
@@ -1,22 +1,56 @@
-import { Article } from "..//Article";
+'use client';
 
-const getArticles = async () => {
+import { useEffect, useState } from "react";
+import styles from "./ArticlesContainer.module.css";
+
+const deleteArticle = async (id: string) => {
     try {
-        const response = await fetch(`${process.env.API_URL}/articles`);
-        const data = await response.json();
-        return data;
+        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/articles/${id}`, {
+            method: "DELETE"
+        });
     } catch (error) {
         console.error(error);
-    } 
+    }
 }
 
-export const ArticlesContainer = async () => {
-    const articles = await getArticles();
-    console.log(articles);
+export const ArticlesContainer = () => {
+    const [articles, setArticles] = useState<Article[]>([]);
+    const [updateArticles, setUpdateArticles] = useState(false);
+
+    const handleDelete = async (event: any, id: string) => {
+        event.preventDefault();
+        await deleteArticle(id);
+        setUpdateArticles(prev => !prev);
+    }
+
+    useEffect(() => {
+        const getArticles = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/articles`);
+                const data = await response.json();
+                setArticles(data);
+            } catch (error) {
+                console.error(error);
+            } 
+        }
+        getArticles();
+    }, [updateArticles]);
+
     return (
         <main>
             {articles.map((article: Article) => (
-                <Article key={article.title} article={article} />
+                <section key={article.title} className={styles.Article}>
+                    <a href={article.url}>
+                        <article>
+                            <div>
+                                <span>{article.title}.</span>
+                                <span className={styles.author}>- {article.author} -</span>
+                            </div>
+                            <div>{article.created_at}</div>
+                            <div onClick={(event) => {handleDelete(event, article._id)}}>🗑️</div>
+                        </article>
+                    </a>
+                </section>
             ))}
         </main>
     )
