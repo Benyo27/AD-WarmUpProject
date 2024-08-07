a838f42a29dd8f387a208615a5192974c74c5c49
Benyo27 <bgonzalezb@uc.cl>
2024-08-07 13:05:04 -0400

    fix(articles module): add config module

--- server/apps/server/src/articles/articles.module.ts
+++ server/apps/server/src/articles/articles.module.ts
@@ -4,9 +4,11 @@ import { ArticlesController } from './articles.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Article, ArticleSchema } from './schema/articles.schema';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
     HttpModule,
   ],
