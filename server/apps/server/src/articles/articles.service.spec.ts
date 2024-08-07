f2511ffeea76669526fff902e0b13aa5a16a1658
Benyo27 <bgonzalezb@uc.cl>
2024-08-06 21:31:53 -0400

    feat(testing): add articles service testing

--- server/apps/server/src/articles/articles.service.spec.ts
+++ server/apps/server/src/articles/articles.service.spec.ts
@@ -1,18 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesService } from './articles.service';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Connection, Model, connect } from 'mongoose';
+import { Article, ArticleSchema } from './schema/articles.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import { HttpModule } from '@nestjs/axios';
 
 describe('ArticlesService', () => {
+  let mongod: MongoMemoryServer;
+  let mongoConnection: Connection;
+  let articleModel: Model<Article>;
+
   let service: ArticlesService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongoConnection = (await connect(uri)).connection;
+    articleModel = mongoConnection.model(Article.name, ArticleSchema);
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticlesService],
+      imports: [HttpModule],
+      providers: [
+        ArticlesService,
+        {
+          provide: getModelToken(Article.name),
+          useValue: articleModel,
+        },
+      ],
     }).compile();
 
     service = module.get<ArticlesService>(ArticlesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterAll(async () => {
+    await mongoConnection.dropDatabase();
+    await mongoConnection.close();
+    await mongod.stop();
+  });
+
+  describe('Article Service Test', () => {
+    it('should fetch and save articles', async () => {
+      await service.fetchAndSaveArticles();
+      const articles = await articleModel.find();
+      expect(articles.length).toBeGreaterThan(0);
+    });
+
+    it('should return all articles', async () => {
+      const articles = await service.findAll();
+      expect(articles.length).toBeGreaterThan(0);
+    });
+
+    it('should delete a certain article', async () => {
+      const article = await articleModel.findOne();
+      const initialArticlesCount = await articleModel.countDocuments();
+      await service.remove(article._id.toString());
+      const finalArticlesCount = await articleModel.countDocuments();
+      expect(finalArticlesCount).toBeLessThan(initialArticlesCount);
+    });
   });
 });
