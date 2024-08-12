c29cdcd34e81b12cd63be7b65cbcbc4533076ef5
Benyo27 <bgonzalezb@uc.cl>
2024-08-12 13:28:48 -0400

    fix(client home): correct ArticlesContainer import path

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
 import { Hero } from "./components/home/Hero";
-import { ArticlesContainer } from "./components/home//ArticlesContainer";
+import { ArticlesContainer } from "./components/home/ArticlesContainer";
 
 export default function Home() {
   return (
